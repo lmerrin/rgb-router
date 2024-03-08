99c77a3876d2c32b722dbb97695eeefaed6c3b13
Lilia <lilia@student.dev-island.org>
2024-03-07 19:47:48 -1000

    mplement navigation with links to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
 import Red from "./Routes/Red";
 import Green from "./Routes/Green";
 import Blue from "./Routes/Blue";
-import "./App.css";
-
 
 function App() {
+  const navigate = useNavigate();
+  const pageRoutes = ["/", "/green", "/blue"];
+
   return (
     <>
+      <nav className="navigation">
+        <ul className="nav-menu">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/green">Green Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blue">Blue Page</NavLink>
+          </li>
+        </ul>
+        <button
+          onClick={() => {
+            const randomPageIndex = Math.floor(
+              Math.random() * pageRoutes.length
+            );
+            navigate(pageRoutes[randomPageIndex]);
+          }}
+        >
+          Random Page
+        </button>
+      </nav>
       <Routes>
         <Route path="/" element={<Red />} />
         <Route path="/green" element={<Green />} />
